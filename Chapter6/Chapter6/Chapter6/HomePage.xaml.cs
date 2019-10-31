67c22e54820085aca70da479534b1ea44784e797
ducthang200398 <trducthang0603@yahoo.com>
2019-10-31 20:45:33 +0700

    bai tap tuan 3

---
+++ Chapter6/Chapter6/Chapter6/HomePage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chapter6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            BindingContext = new HomePageViewModel(Navigation);
+        }
+    }
+
+
+    public class HomePageViewModel : BindableObject
+    {
+
+        private readonly INavigation navigation;
+
+        public Command<string> NavigateCommand
+        {
+            get;
+            set;
+        }
+
+        public HomePageViewModel(INavigation navigation)
+        {
+            this.navigation = navigation;
+            NavigateCommand = new Command<string>(NagivigateRequested);
+        }
+
+
+        async void NagivigateRequested(string pageName)
+        {
+            var pageType = Type.GetType("Chapter6." + pageName);
+            Page page = (Page)Activator.CreateInstance(pageType);
+            await navigation.PushAsync(page);
+        }
+    }
+}
